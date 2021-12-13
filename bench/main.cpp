74e3f27ae2dec3a439ae7597a8fa55c20598d6cb
Bronek Kozicki <brok@incorrekt.com>
2021-12-13 21:24:25 +0000

    Working tests

--- bench/main.cpp
+++ bench/main.cpp
@@ -1,10 +1,151 @@
+#include "queue.hpp"
+
+#include <atomic>
+#include <bitset>
+#include <chrono>
+#include <stdexcept>
+#include <thread>
+#include <vector>
+
 #include <cstdint>
+#include <cstdio>
+#include <cstring>
+
+#include <pthread.h> // for pthread_getaffinity_np
 
 struct config {
-    uint64_t cpus;
-    uint64_t size;
+  std::bitset<64> cpus;
+  uint64_t size;
+
+  friend auto parse(config &dest, std::string c, std::string s) noexcept
+      -> bool {
+    if (c.empty() || s.empty()) {
+      return false;
+    }
+
+    try {
+      dest.cpus = decltype(dest.cpus){c};
+
+      decltype(config::size) multiplier = 1;
+      auto last = --s.end();
+      switch (*last) {
+      case 'k':
+        multiplier = 1000;
+        s.erase(last, s.end());
+        break;
+      case 'K':
+        multiplier = 1024;
+        s.erase(last, s.end());
+        break;
+      case 'm':
+        multiplier = 1000'000;
+        s.erase(last, s.end());
+        break;
+      case 'M':
+        multiplier = 1024 * 1024;
+        s.erase(last, s.end());
+        break;
+      }
+      size_t n = 0;
+      dest.size = stoull(s, &n);
+      if (n != s.size()) {
+        throw std::invalid_argument("Unable to parse " + s);
+      }
+      dest.size *= multiplier;
+    } catch (std::exception const &e) {
+      std::fprintf(stderr, "Bad program argument: %s\n\n", e.what());
+      return false;
+    }
+
+    std::printf("Cores: %s (%lu)\n", dest.cpus.to_string().c_str(),
+                dest.cpus.count());
+    std::printf("Size: %lu\n", dest.size);
+    return true;
+  }
 };
 
+auto run(config const &c) noexcept -> int {
+  using namespace std;
+  vector<thread> threads;
+  std::atomic<uint64_t> started = {0};
+  std::atomic<int> starter = {0};
+  std::atomic<uint64_t> completed = {0};
+  struct dummy {
+    constexpr dummy() noexcept = default;
+    long payload[7] = {};
+  };
+
+  queue_t<dummy> queue{c.size};
+
+  for (size_t i = 0; i < c.cpus.size(); ++i) {
+    if (!c.cpus.test(i)) {
+      continue;
+    }
+
+    threads.emplace_back([&, cpu = i, count = (int)c.cpus.count(),
+                          size = (int64_t)c.size]() noexcept {
+      cpu_set_t cpuset;
+      CPU_ZERO(&cpuset);
+      CPU_SET(cpu, &cpuset);
+      pthread_getaffinity_np(pthread_self(), sizeof(cpuset), &cpuset);
+      std::printf("thread %lu on %li\n", pthread_self(), cpu);
+
+      started += 1;
+
+      // spin to ensure all threads kick off at the same time
+      while (starter.load(std::memory_order_relaxed) == 0) {
+      }
+
+      while (enqueue(queue, dummy{},
+                     [size](int64_t count) noexcept { return count < size; })) {
+      }
+
+      completed += 1;
+    });
+  }
+
+  while (started < c.cpus.count()) {
+  }
+
+  starter = 1;
+  auto const start = std::chrono::high_resolution_clock::now();
+
+  while (completed < c.cpus.count()) {
+  }
+  auto const done = std::chrono::high_resolution_clock::now();
+  std::printf("Result: %g\n", double(done.time_since_epoch().count() -
+                                     start.time_since_epoch().count()) /
+                                  c.size);
+
+  for (auto &t : threads) {
+    t.join();
+  }
+  return 0;
+}
+
+void usage(char const *p_) noexcept {
+  using namespace std;
+  puts("Proposal P0493 benchmark runner\n\nExample usage:\n");
+  printf("%s -c 1110 -s 100K\n\n", p_);
+  puts("Where:\n-c cores to run on (as parsed by bitset ctor)");
+  puts("-s size of the queue to fill, with optional multiplier suffix: k=1e3, "
+       "K=2^10, m=1e6, M=2^20, g=1e9, G=2^30\n");
+  puts("The example above will fill 102400 large queue using 3 threads, "
+       "running on cores 1, 2, and 3 (skipping core 0)\n");
+}
+
 auto main(int argc, char **argv) noexcept -> int {
-    
+  using namespace std;
+  if (argc == 5 &&
+      (strlen(argv[1]) == 2 && argv[1][0] == '-' && argv[1][1] == 'c') &&
+      (strlen(argv[3]) == 2 && argv[3][0] == '-' && argv[3][1] == 's')) {
+    config c = {};
+    if (parse(c, argv[2], argv[4])) {
+      return run(c);
+    }
+  } else {
+    usage(argv[0]);
+  }
+
+  return 1;
 }
--- bench/queue.hpp
+++ bench/queue.hpp
@@ -1,56 +1,84 @@
 #include <atomic>
 #include <type_traits>
+#include <vector>
+
 #include <cstdint>
+#include <cstdio>
 
 #include "fetch_max.hpp"
 
-template <typename T, uint64_t Size>
-struct queue_t {
-  static_assert(std::is_nothrow_default_constructible<T>::value);
-  static_assert(std::is_nothrow_copy_constructible<T>::value);
-  static_assert(std::is_nothrow_swappable<T>::value);
+template <typename T> struct queue_t {
+  static_assert(std::is_nothrow_default_constructible_v<T>);
+  static_assert(std::is_nothrow_copy_constructible_v<T>);
+  static_assert(std::is_nothrow_swappable_v<T>);
 
   using element_t = T;
-  static constexpr uint64_t size = Size;
+  const uint64_t size;
 
   struct entry_t {
-    element_t item {};                            // a queue element
-    std::atomic<int> tag {-1};              // its generation number
+    element_t item{}; // a queue element
+    int tag{-1};      // its generation number
   };
+  using tag_t = std::atomic_ref<int>;
 
-  entry_t elts[size] = {};                    // a bounded array
-  std::atomic<int> back {-1};
+  std::vector<entry_t> elts;
+  explicit queue_t(uint64_t size) noexcept : size(size) {
+    // will crash if we cannot allocate enough memory - this is good
+    elts.resize(size, entry_t{});
+    // visit elements to ensure the memory is 1. allocated 2. paged in and
+    // 3. TLB up-to-date
+    int d = 1;
+    for (size_t i = 0; i < elts.size(); i += 32) {
+      auto& e = elts[i];
+      d *= (int volatile)e.tag;
+    }
+    std::printf("Check: %i\n", d);
+  }
 
-  friend void enqueue(queue_t& queue, element_t x) noexcept {
-    int i = queue.back.load() + 1;          // get a slot in the array for the new element
+  std::atomic<int64_t> back{-1};
+
+  template <typename Full_>
+  friend auto enqueue(queue_t &queue, element_t x, Full_ const &fn) noexcept -> bool {
+    // get a slot in the array for the new element
+    int i = queue.back.load(std::memory_order_relaxed) + 1;
     while (true) {
-      // exchange the new element with slots value if that slot has not been used
-      int empty = -1;                       // expected tag for an empty slot
-      auto& e = queue.elts[i % size];
-      // use two-step write: first store an odd value while we are writing the new element
-      if (std::atomic_compare_exchange_strong(&e.tag, &empty, (i / size) * 2 + 1)) {
+      // expected tag for an empty slot
+      int empty = -1;
+      auto &e = queue.elts[i % queue.size];
+      auto tag = tag_t(e.tag);
+      // first store an odd value while we are writing the new element
+      if (tag.compare_exchange_strong(empty, (i / queue.size) * 2 + 1,
+                                      std::memory_order_relaxed, //
+                                      std::memory_order_relaxed)) {
         using std::swap;
         swap(x, e.item);
-        e.tag.store((i / size) * 2);        // done writing, switch tag to even (ie. ready)
+        // done writing, switch tag to even (ie. ready)
+        tag.store((i / queue.size) * 2, std::memory_order_seq_cst);
         break;
+      } else if (!fn(i)) {
+        // are we done yet?
+        return false;
       }
       ++i;
     }
-    atomic_fetch_max(&queue.back, i);  // reset the value of back
+    // reset the value of back
+    atomic_fetch_max_explicit(&queue.back, i, std::memory_order_relaxed);
+    return true;
   }
 
-  friend auto dequeue(queue_t& queue) noexcept -> element_t {
-    while (true) {                          // keep trying until an element is found
-      int range = queue.back.load();        // search up to back slots
+  friend auto dequeue(queue_t &queue) noexcept -> element_t {
+    while (true) {                   // keep trying until an element is found
+      int range = queue.back.load(); // search up to back slots
       for (int i = 0; i <= range; i++) {
-        int ready = (i / size) * 2;         // expected even tag for ready slot
-        auto& e = queue.elts[i % size];
+        int ready = (i / queue.size) * 2; // expected even tag for ready slot
+        auto &e = queue.elts[i % queue.size];
+        auto tag = tag_t(e.tag);
         // use two-step read: first store -2 while we are reading the element
-        if (std::atomic_compare_exchange_strong(&e.tag, &ready, -2)) {
+        if (tag.compare_exchange_strong(ready, -2)) {
           using std::swap;
           element_t ret{};
           swap(ret, e.item);
-          e.tag.store(-1);                  // done reading, switch tag to -1 (ie. empty)
+          tag.store(-1); // done reading, switch tag to -1 (ie. empty)
           return ret;
         }
       }
