4afab948724f95083b1cd57d1c38f6f64b824dca
Bronek Kozicki <brok@incorrekt.com>
2021-12-14 21:17:38 +0000

    Add calibration to bench2

--- bench2/runner.hpp
+++ bench2/runner.hpp
@@ -59,7 +59,7 @@ template <type_e Impl_> struct runner final {
           // Choose a random mean between 1 and 6
           std::ranlux24 r{seed + cpu};
           std::uniform_int_distribution<int> dist(-1e9, 1e9);
-          // warm up random number generator
+          // warm up PRNG
           for (int i = 0; i < 100; ++i) {
             fetch_max(&max, dist(r), std::memory_order_relaxed);
           }
@@ -94,16 +94,46 @@ template <type_e Impl_> struct runner final {
       }
     } // join all threads
 
-    stats s{};
+    double minimum_3s = 0; // three sigma minimum cost of PRNG
+    double max_s = 0.5;    // maximum sigma to complete calibration
+    while (true) {
+      std::ranlux24 r{(std::size_t)seed};
+      std::uniform_int_distribution<int> dist(-1e9, 1e9);
+      // warm up PRNG
+      for (int j = 0; j < 100; ++j) {
+        (void)dist(r);
+      }
+
+      static constexpr int calib_inner_iters = 10000;
+      stats s1{};
+      for (int i = 0; i < 100; ++i) {
+        auto const start = std::chrono::high_resolution_clock::now();
+        for (int j = 0; j < calib_inner_iters; ++j) {
+          (void)dist(r);
+        }
+        auto const done = std::chrono::high_resolution_clock::now();
+        auto const time = (double)done.time_since_epoch().count() -
+                          start.time_since_epoch().count();
+        s1.push(time / (double)calib_inner_iters);
+      }
+
+      if (s1.stdev() < max_s) {
+        minimum_3s = std::max(0.0, (s1.mean() - s1.stdev() * 3));
+        std::fprintf(::stderr, "Calibration: %g\n\n", minimum_3s);
+        break;
+      }
+    }
+
+    stats s2{};
     for (size_t i = 0; i < cpus.size(); ++i) {
       if (!cpus.test(i)) {
         continue;
       }
       auto const r = results[i];
-      s.push(r);
+      s2.push(r - minimum_3s);
     }
 
-    std::printf("%lu\t%g\t%g\n", cpus.count(), s.mean(), s.stdev());
+    std::printf("%lu\t%g\t(%g)\n", cpus.count(), s2.mean(), s2.stdev());
     return 0;
   }
 };
