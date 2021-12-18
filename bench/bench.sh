4fbfd425c563bc2664de89973e401719b4afa8ef
Bronek Kozicki <brok@incorrekt.com>
2021-12-18 22:06:26 +0000

    Fix bench.sh

--- bench/bench.sh
+++ bench/bench.sh
@@ -2,8 +2,11 @@
 
 DIR=$(dirname "$BASH_SOURCE[0]")
 DIR=$(realpath "$DIR")
-while :; do
+NUM=$1
+shift
+"$DIR"/bench -c 4 $@ -i 10000 2>&1 1>/dev/null | grep -vE "iterations|Calibration|seed|Will use|core\(s\)|^$"
   for i in 1 2 4 8 16 24 32 40 48 56 64; do
-    "$DIR"/bench2 -c $i $@ 2>/dev/null
-  done
+    for j in $(seq $NUM); do
+      "$DIR"/bench -c $i $@ 2>/dev/null
+    done
 done
